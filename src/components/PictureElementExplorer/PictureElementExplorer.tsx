9086ee04534bda91dc7e9bc22eb7e15e70414ee4
S'unya <sunya.dickman@aerian.com>
2019-11-18 22:01:00 +0000

    Working towards basic image

--- src/components/PictureElementExplorer/PictureElementExplorer.tsx
+++ src/components/PictureElementExplorer/PictureElementExplorer.tsx
@@ -2,19 +2,38 @@ import * as React from "react";
 import { DisplayImageProps, SourceImageProps } from "../../pages";
 
 const calculateSizes = ({ maxWidth }: DisplayImageProps): string => {
-    return `(max-width: ${maxWidth}) 100vw, ${maxWidth}`;
+    return `(max-width: ${maxWidth || "800px"}) 100vw, ${maxWidth || "800px"}`;
 };
 const calculateSrcSet = (
-    displayImageProps: DisplayImageProps,
-    sourceImageProps: SourceImageProps
+    { displayBreakpoints, maxWidth, displayType },
+    { width }
 ): [string, string][] => {
-    const srcSet = [];
+    // this is only for fluid so far
+    const baseWidth = maxWidth || 800;
+    const breakPoints = displayBreakpoints.split(", ");
+    let srcSet;
+    let lastNumberStr: boolean = false;
+
+    if (breakPoints.length) {
+        srcSet = breakPoints.reduce((acc, bp: string) => {
+            if (((bp as unknown) as number) * 1 < width) {
+                acc.push(`/static/path/to/image.jpg ${bp}w`);
+            }
+
+            if (!lastNumberStr) {
+                acc.push(`/static/path/to/image.jpg ${width}w`);
+                lastNumberStr = true;
+            }
+
+            return acc;
+        }, []);
+    }
 
     return srcSet;
 };
 const calculateSrc = (
     srcSet: [string, string][],
-    sourceImageProps: SourceImageProps
+    displayImageProps: DisplayImageProps
 ): string => {
     let src = "";
 
@@ -25,32 +44,27 @@ interface Props extends React.HTMLAttributes<HTMLElement> {
     sourceImageProps: SourceImageProps;
 }
 
-export const PictureElementExplorer: React.FC<Props> = ({
-    displayImageProps: DisplayImageProps,
-    sourceImageProps: SourceImageProps
-}) => {
+export const PictureElementExplorer: React.FC<Props> = props => {
+    const { displayImageProps, sourceImageProps } = props;
     const sizes = calculateSizes(displayImageProps);
     const srcSet = calculateSrcSet(displayImageProps, sourceImageProps);
     const src = calculateSrc(srcSet, displayImageProps);
 
     return (
-        <code>
-            <div className="gatsby-image-wrapper">
-                <picture>
-                    <source />
-                    <img
-                        srcSet={srcSet
-                            .map(resourceData => {
-                                return resourceData.join(" ");
-                            })
-                            .join(", ")}
-                        src={src}
-                        alt=""
-                        sizes={sizes}
-                    />
-                </picture>
-            </div>
-        </code>
+        <pre>
+            {`
+<div className="gatsby-image-wrapper">
+    <picture>
+        <source /><!-- todo -->
+        <img
+            srcSet="${srcSet.join(", ")}"
+            src="${src}"
+            alt=""
+            sizes="${sizes}"
+        />
+    </picture>
+</div>`}
+        </pre>
     );
 };
 export default PictureElementExplorer;
