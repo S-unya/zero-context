780c6ed96f903b4a311ab5ffc09109085083fb8c
Sunya Dickman <sunya.dickman@aerian.com>
2019-12-10 20:08:42 +0000

    Change tack to auto-reap image info

--- src/components/ImageForm/ImageForm.tsx
+++ src/components/ImageForm/ImageForm.tsx
@@ -1,258 +1,115 @@
-import * as React from "react";
-import { SourceImageProps, DisplayImageProps } from "../../pages";
+import React from "react";
+import { SourceImageProps } from "../../pages";
 
 export enum FormFieldType {
-    // display parameters
-    "MAXWIDTH" = "maxWidth",
-    "MAXHEIGHT" = "maxHeight",
-    "DISPLAYTYPE" = "displayType",
-    "QUALITY" = "quality",
-    "IMGBG" = "imageBackground",
-    "FIT" = "fit",
-    "BRKPNTS" = "displayBreakpoints",
-    // Source image parameters
     "SOURCEWIDTH" = "sourceImageWidth",
     "SOURCEHEIGHT" = "sourceImageHeight",
-    "SOURCETYPE" = "sourceImageType"
+    "SOURCETYPE" = "sourceImageType",
+    "SOURCENAME" = "sourceImageName"
 }
 
 interface Props extends React.FormHTMLAttributes<HTMLFormElement> {
     sourceImageProps: SourceImageProps;
-    displayImageProps: DisplayImageProps;
     updateSourceImageProps: (
         f: (draft: SourceImageProps) => void | SourceImageProps
     ) => void;
-    updateDisplayImageProps: (
-        f: (draft: DisplayImageProps) => void | DisplayImageProps
-    ) => void;
-    currentFocus: FieldType;
 }
 
 // @TODO: add fragments support
 export const ImageForm: React.FC<Props> = ({
     sourceImageProps,
-    displayImageProps,
-    updateDisplayImageProps,
-    updateSourceImageProps,
-    currentFocus
+    updateSourceImageProps
 }) => {
-    function setImageType(event: React.ChangeEvent<HTMLSelectElement>) {
-        updateSourceImageProps((draft: SourceImageProps) => {
-            const type = event.currentTarget.value;
+    const canvasRef = React.useRef<undefined | HTMLCanvasElement>();
 
-            draft.type = type;
-        });
-    }
-    function setImageWidth(event: React.ChangeEvent<HTMLInputElement>) {
-        updateSourceImageProps((draft: SourceImageProps) => {
-            const width = parseInt(event.target.value, 10);
+    const setImage = React.useCallback(
+        async (event: React.ChangeEvent<HTMLInputElement>) => {
+            const target: HTMLInputElement = event.nativeEvent
+                .target as HTMLInputElement;
+            const files: FileList =
+                typeof target.files !== "undefined" && target.files;
+            const getInfoFromUpload = (
+                file: File
+            ): Promise<SourceImageProps> => {
+                return new Promise((resolve, reject) => {
+                    try {
+                        const imageProps: SourceImageProps = {
+                            name: file.name,
+                            fileType: file.type.replace("image/", ""),
+                            width: null,
+                            height: null
+                        };
+                        const fileReader = new FileReader();
+                        fileReader.readAsDataURL(file);
+                        fileReader.onload = () => {
+                            const img = new Image();
 
-            draft.width = width;
-        });
-    }
-    function setImageHeight(event: React.ChangeEvent<HTMLInputElement>) {
-        updateSourceImageProps((draft: SourceImageProps) => {
-            const height = parseInt(event.target.value, 10);
+                            // @todo: may wish to save this for making a real image example
+                            img.src = fileReader.result as string;
+                            imageProps.width = img.width;
+                            imageProps.height = img.height;
 
-            draft.height = height;
-        });
-    }
-    function setImageDisplayType(event: React.ChangeEvent<HTMLInputElement>) {
-        const type: "fluid" | "fixed" | undefined = event.target.value as
-            | "fluid"
-            | "fixed"
-            | undefined;
+                            resolve(imageProps);
+                        };
+                    } catch (err) {
+                        reject(err);
+                    }
+                });
+            };
 
-        updateDisplayImageProps((draft: DisplayImageProps) => {
-            draft.displayType = type;
-        });
-    }
-    function setDisplayImageWidth(event: React.ChangeEvent<HTMLInputElement>) {
-        const width = parseInt(event.target.value, 10);
+            if (files.length) {
+                const imageInfo = await getInfoFromUpload(files[0]);
 
-        updateDisplayImageProps((draft: DisplayImageProps) => {
-            draft.maxWidth = width;
-        });
-    }
-    function setDisplayImageHeight(event: React.ChangeEvent<HTMLInputElement>) {
-        const height = parseInt(event.target.value, 10);
-
-        updateDisplayImageProps((draft: DisplayImageProps) => {
-            draft.maxHeight = height;
-        });
-    }
-    function setDisplayImageQuality(
-        event: React.ChangeEvent<HTMLInputElement>
-    ) {
-        const quality = parseInt(event.target.value, 10);
-
-        updateDisplayImageProps((draft: DisplayImageProps) => {
-            draft.quality = quality;
-        });
-    }
-    function setDisplayImageFit(event: React.ChangeEvent<HTMLInputElement>) {
-        updateDisplayImageProps((draft: DisplayImageProps) => {
-            const fit = event.target.value;
-
-            draft.fit = fit;
-        });
-    }
-    function setDisplayImageBreakpoints(
-        event: React.ChangeEvent<HTMLInputElement>
-    ) {
-        const displayBreakpoints = event.target.value;
-
-        updateDisplayImageProps((draft: DisplayImageProps) => {
-            draft.displayBreakpoints = displayBreakpoints;
-        });
-    }
-
-    const outPutImageTypeFields = () => {
-        if (!displayImageProps.displayType) {
-            return null;
-        }
-
-        const baseProperties = (
-            <div key="base-props">
-                <div className="input-wrap">
-                    <label htmlFor="image-width">Display width</label>{" "}
-                    <input
-                        type="number"
-                        value={displayImageProps.maxWidth || ""}
-                        onChange={setDisplayImageWidth}
-                        placeholder="800"
-                    />
-                </div>
-                <div className="input-wrap">
-                    <label htmlFor="image-height">Display height</label>{" "}
-                    <input
-                        type="number"
-                        value={displayImageProps.maxHeight || ""}
-                        onChange={setDisplayImageHeight}
-                    />
-                </div>
-                <div className="input-wrap">
-                    <label htmlFor="image-quality">
-                        Image compression quality
-                    </label>{" "}
-                    <input
-                        type="number"
-                        value={displayImageProps.quality || ""}
-                        onChange={setDisplayImageQuality}
-                        max="100"
-                        min="0"
-                        placeholder="50"
-                    />
-                </div>
-            </div>
-        );
-
-        const fluidProperties = (
-            <div key="fluid-props">
-                <div className="input-wrap">
-                    <label htmlFor="image-breakpoints">
-                        Display image sizes (comma separated)
-                    </label>{" "}
-                    <input
-                        type="text"
-                        value={displayImageProps.displayBreakpoints || ""}
-                        onChange={setDisplayImageBreakpoints}
-                        placeholder="800"
-                    />
-                </div>
-            </div>
-        );
-
-        return (
-            <fieldset>
-                {displayImageProps.displayType === "fixed"
-                    ? baseProperties
-                    : [baseProperties, fluidProperties]}
-            </fieldset>
-        );
-    };
+                updateSourceImageProps((draft: SourceImageProps) => {
+                    draft.width = imageInfo.width;
+                    draft.height = imageInfo.height;
+                    draft.fileType = imageInfo.fileType;
+                    draft.name = imageInfo.name;
+                });
+            }
+        },
+        []
+    );
 
     return (
         <form>
             <fieldset>
-                <legend>About your image</legend>
+                <legend>Your image</legend>
                 <p>
-                    These options are about your source image, they would
-                    normally be read directly from the file by{" "}
-                    <code>gatsby-plugin-sharp</code>
+                    This is used to get information about your image. It stays
+                    on your computer.
                 </p>
                 <div className="input-wrap">
-                    <label htmlFor="source-image-width">Width in pixels:</label>{" "}
+                    <label htmlFor="source-image">
+                        Your hi-res source image:
+                    </label>{" "}
                     <input
-                        type="number"
-                        id="source-image-width"
-                        value={sourceImageProps.width || ""}
-                        onChange={setImageWidth}
+                        type="file"
+                        id="source-image"
+                        onChange={setImage}
+                        accept="image/png, image/jpg, image/webp"
                     />{" "}
-                    <span>px</span>
                 </div>
-                <div className="input-wrap">
-                    <label htmlFor="source-image-height">
-                        height in pixels:
-                    </label>{" "}
-                    <input
-                        type="number"
-                        id="source-image-height"
-                        value={sourceImageProps.height || ""}
-                        onChange={setImageHeight}
-                    />{" "}
-                    <span>px</span>
-                </div>
-                <div className="input-wrap">
-                    <label htmlFor="source-image-type">
-                        Type of source image:
-                    </label>{" "}
-                    <select
-                        id="source-image-type"
-                        name="source-image-type"
-                        onChange={setImageType}
-                    >
-                        <option value="">-- Choose --</option>
-                        <option value="jpg">.jpg</option>
-                        <option value="webp">.webp</option>
-                    </select>
-                </div>
-                <p>maybe put a src here</p>
+                <table>
+                    <tr>
+                        <th>Name:</th>
+                        <td>{sourceImageProps.name}</td>
+                    </tr>
+                    <tr>
+                        <th>Width:</th>
+                        <td>{sourceImageProps.width}</td>
+                    </tr>
+                    <tr>
+                        <th>Height:</th>
+                        <td>{sourceImageProps.height}</td>
+                    </tr>
+                    <tr>
+                        <th>File type:</th>
+                        <td>{sourceImageProps.fileType}</td>
+                    </tr>
+                </table>
             </fieldset>
-            <fieldset>
-                <legend>How will the image display?</legend>
-                <p>
-                    These options are about how you want your image to display,
-                    there is more help available for each field.
-                </p>
-                <div className="input-wrap">
-                    <label htmlFor="image-type-fixed">
-                        Fixed size (with appropriate images for hi-res screens):
-                    </label>{" "}
-                    <input
-                        type="radio"
-                        id="image-type-fixed"
-                        name="image-type"
-                        value="fixed"
-                        checked={displayImageProps.displayType === "fixed"}
-                        onChange={setImageDisplayType}
-                    />
-                    <br />
-                    <label htmlFor="image-type-fluid">
-                        Fluid size (adapts to the width of the screen):
-                    </label>{" "}
-                    <input
-                        type="radio"
-                        id="image-type-fluid"
-                        name="image-type"
-                        value="fluid"
-                        checked={displayImageProps.displayType === "fluid"}
-                        onChange={setImageDisplayType}
-                    />
-                    <div className="help">Some help here</div>
-                </div>
-            </fieldset>
-            {outPutImageTypeFields()}
+            <canvas ref={canvasRef} />
         </form>
     );
 };
