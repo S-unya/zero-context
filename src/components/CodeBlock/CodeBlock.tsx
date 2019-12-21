b85b5c5ad0fb53949cb3fd83fa1af9b1a5712b4f
Sunya Dickman <sunya.dickman@aerian.com>
2019-12-21 16:31:32 +0000

    Change of plan, going simple to start

--- src/components/CodeBlock/CodeBlock.tsx
+++ src/components/CodeBlock/CodeBlock.tsx
@@ -1,52 +1,23 @@
-/* eslint react/jsx-key: 0 */
+import * as React from "react";
 
-import React from "react";
-import Highlight, { defaultProps } from "prism-react-renderer";
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
 
-interface Props extends React.AllHTMLAttributes<HTMLElement> {
-    live: boolean;
-    changeHandler: (code: string) => void;
+interface Props extends React.HTMLAttributes<HTMLElement> {
 }
-export const CodeBlock: React.FC<Props> = ({
-    children,
-    className,
-    live,
-    changeHandler
-}) => {
-    const language = className.replace(/language-/, "");
 
-    if (live) {
-        return (
-            <div style={{ marginTop: "40px", backgroundColor: "black" }}>
-                <LiveProvider code={children.trim()}>
-                    <LivePreview />
-                    <LiveEditor onChange={changeHandler} />
-                    <LiveError />
-                </LiveProvider>
-            </div>
-        );
-    }
+export const CodeBlock: React.FC<Props> = ({ children, style, className }) => {
+    // STATE
+    // STATE:END
 
+    // CALLBACKS
+    // CALLBACKS:END
+
+    // EFFECT
+    // EFFECT:END
+    
     return (
-        <Highlight {...defaultProps} code={children.trim()} language={language}>
-            {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                <pre
-                    className={className}
-                    style={{ ...style, padding: "20px" }}
-                >
-                    {tokens.map((line, i) => (
-                        <div key={i} {...getLineProps({ line, key: i })}>
-                            {line.map((token, key) => (
-                                <span
-                                    key={key}
-                                    {...getTokenProps({ token, key })}
-                                />
-                            ))}
-                        </div>
-                    ))}
-                </pre>
-            )}
-        </Highlight>
+    <div style={style}>
+        {children}
+    </div>
     );
 };
+export default CodeBlock;
