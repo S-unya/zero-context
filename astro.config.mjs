c460748202f2dae967b95e100ed8781e33dc37cb
S'unya <6469947+S-unya@users.noreply.github.com>
2026-04-22 20:24:43 +0100

    chore: cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,34 +3,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import remarkToc from "remark-toc";
-import copy from "rollup-plugin-copy";
 import { remarkReadingTime } from "./src/transforms/remarkReadingTime.ts";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://zero-context.com",
-    integrations: [mdx(), sitemap()],
-    markdown: {
-        remarkPlugins: [[remarkToc, { heading: "contents" }], remarkReadingTime],
-        shikiConfig: {
-            wrap: true,
-            langs: ["javascript" , "typescript", "css", "html", "json", "scss"],
-        },
-    },
-    responsiveImages: true,
-    vite: {
-        plugins: [
-            copy({
-                // Copy only on first build. We don't want to trigger additional server reloads.
-                copyOnce: true,
-                hook: "buildStart",
-                targets: [
-                    {
-                        src: "node_modules/@shoelace-style/shoelace/dist/assets/*",
-                        dest: "public/shoelace-assets/assets/",
-                    },
-                ],
-            }),
-        ],
-    },
+	site: "https://zero-context.com",
+	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [[remarkToc, { heading: "contents" }], remarkReadingTime],
+		shikiConfig: {
+			wrap: true,
+		},
+	},
 });
